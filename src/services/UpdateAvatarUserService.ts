f488e1841db56f8ff0fc65b3d8aedd1402bf3aa0
João Eliandro <eliandrogermano@gmail.com>
2020-07-11 05:37:36 -0300

    Created Update avatar user service

---
+++ src/services/UpdateAvatarUserService.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface RequestDTO {
+    user_id: string;
+    avatarFileName: string;
+}
+
+class UpdateAvatarUserService {
+    public async execute({
+        user_id,
+        avatarFileName,
+    }: RequestDTO): Promise<User> {
+        const userRepository = getRepository(User);
+
+        const user = await userRepository.findOne(user_id);
+
+        if (!user)
+            throw new Error('Only authenticated users can change avatar.');
+
+        if (user.avatar) {
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+
+            const userAvatarFileExists = await fs.promises.stat(
+                userAvatarFilePath,
+            );
+
+            if (userAvatarFileExists)
+                await fs.promises.unlink(userAvatarFilePath);
+        }
+
+        user.avatar = avatarFileName;
+
+        await userRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateAvatarUserService;
